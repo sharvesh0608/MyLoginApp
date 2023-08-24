87ec838853b6e8c971355da2060f73c07aa39104
sharvesh <sharveshsreethar0806@gmail.com>
2023-08-24 20:35:21 +0530

    Added Toast

--- app/src/main/java/com/sharveshapps/myloginapp/MainActivity.java
+++ app/src/main/java/com/sharveshapps/myloginapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.sharveshapps.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText email,password;
+        email= (EditText) findViewById(R.id.editTextEmail);
+        password= (EditText)findViewById(R.id.editTextPassword);
+        Bundle bundle = getIntent().getExtras();
+            String Email = bundle.getString("Email");
+            String Password = bundle.getString("Pass");
+            email.setText(Email);
+            password.setText(Password);
+        Button LoginBtn44 = (Button)findViewById(R.id.loginBtn3);
+            LoginBtn44.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
 
     }
+
 }
--- app/src/main/java/com/sharveshapps/myloginapp/signup.java
+++ app/src/main/java/com/sharveshapps/myloginapp/signup.java
@@ -6,22 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
+    Button signupBtn;
+    EditText name, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Button signupBtn=(Button)findViewById(R.id.loginBtn);
+        signupBtn = (Button) findViewById(R.id.signBtn);
+        name = (EditText) findViewById(R.id.editTextEmail);
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        password = (EditText) findViewById(R.id.editTextPassword);
+
+
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                String Email = email.getText().toString();
+                String Pass = password.getText().toString();
+                Bundle bundle = new Bundle();
+                bundle.putString("Email", Email);
+                bundle.putString("Pass", Pass);
+                Intent intent = new Intent(signup.this, MainActivity.class);
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
+
+
+
+
 
     }
 }
