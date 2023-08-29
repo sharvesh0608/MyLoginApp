9cba713aa7054e562e55015e6ee607cf1e4176ef
sharvesh <sharveshsreethar0806@gmail.com>
2023-08-29 12:39:35 +0530

    LOGOUT implemented

--- app/src/main/java/com/sharveshapps/myloginapp/Home.java
+++ app/src/main/java/com/sharveshapps/myloginapp/Home.java
@@ -1,25 +1,67 @@
-package com.sharveshapps.myloginapp;
+    package com.sharveshapps.myloginapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.recyclerview.widget.LinearLayoutManager;
+    import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.content.SharedPreferences;
+    import android.graphics.Color;
+    import android.os.Build;
+    import android.os.Bundle;
+    import android.view.Menu;
+    import android.view.MenuItem;
+    import android.view.View;
+    import android.widget.PopupMenu;
 
-import java.util.List;
+    import java.util.List;
 
-public class Home extends AppCompatActivity {
+    public class Home extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
-        List<User> userList = databaseHelper.getAllUsers();
-        UserAdapter userAdapter = new UserAdapter(userList);
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(userAdapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home);
+            DatabaseHelper databaseHelper = new DatabaseHelper(this);
+            List<User> userList = databaseHelper.getAllUsers();
+            UserAdapter userAdapter = new UserAdapter(userList);
+            RecyclerView recyclerView = findViewById(R.id.recyclerView);
+            recyclerView.setAdapter(userAdapter);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(layoutManager);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+            }
+
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_home, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+
+            if (id == R.id.action_logout) {
+                // Clear shared preferences
+                SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+
+                // Navigate to Login Activity
+                Intent intent = new Intent(this, LogIn.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
     }
-}
